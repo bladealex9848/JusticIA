6ca721be06382d76655cd24fe77ea7e4e592b470
Alexander Oviedo Fadul <bladealex@gmail.com>
2024-09-11 06:28:37 -0500

    Commit inicial

---
+++ README.md
@@ -0,0 +1,51 @@
+![Logo de JusticIA](https://github.com/bladealex9848/JusticIA/blob/main/assets/logo.jpg)
+
+# JusTicIA: Transformando la Justicia Colombiana con Inteligencia Artificial Ética 🤖⚖️
+
+## Descripción
+JusticiaIA es un agente de IA especializado en la transformación digital del sistema judicial colombiano y la aplicación ética de la inteligencia artificial en este ámbito. 
+
+## Funcionalidades
+* Explica cómo la IA está modernizando la justicia colombiana
+* Describe beneficios y desafíos de la IA en la justicia
+* Brinda información sobre proyectos e iniciativas de IA
+* Discute consideraciones éticas y legales
+* Explora el futuro de la IA en el sistema judicial
+
+## Limitaciones
+* No brinda asesoramiento legal vinculante
+* Conocimiento basado en información pública hasta septiembre 2023
+* No tiene acceso a información privilegiada o confidencial
+* Se recomienda consultar expertos legales y tecnológicos
+
+## Instalación
+
+1. Asegúrate de tener Python 3.8 o superior instalado en tu máquina.
+2. Clona este repositorio: `git clone https://github.com/bladealex9848/JusticIA.git`
+3. Navega al directorio del proyecto: `cd JusticIA`
+4. Instala las dependencias: `pip install -r requirements.txt`
+5. Crea un archivo `.streamlit/secrets.toml` y añade tu ASSISTANT_ID de OpenAI:
+   ```
+   ASSISTANT_ID = "tu-assistant-id-aqui"
+   ```
+6. Configura tu clave API de OpenAI como variable de entorno o en el archivo `secrets.toml`.
+
+## Uso
+
+1. Ejecuta la aplicación: `streamlit run app.py`
+2. Abre tu navegador y ve a `http://localhost:8501`
+3. Comienza a interactuar con el Asistente Virtual escribiendo tus preguntas o solicitudes.
+
+## Contribuciones
+
+Las contribuciones son bienvenidas. Por favor, haz un fork del repositorio, crea una nueva rama para tus cambios, y envía un pull request.
+
+## Licencia
+
+Este proyecto está bajo la licencia MIT. Consulta el archivo `LICENSE` para más detalles.
+
+## Autor
+
+Creado por Alexander Oviedo Fadul
+
+[GitHub](https://github.com/bladealex9848) | [Website](https://alexanderoviedofadul.dev) | [LinkedIn](https://www.linkedin.com/in/alexander-oviedo-fadul/) | [Instagram](https://www.instagram.com/alexander.oviedo.fadul) | [Twitter](https://twitter.com/alexanderofadul) | [Facebook](https://www.facebook.com/alexanderof/) | [WhatsApp](https://api.whatsapp.com/send?phone=573015930519&text=Hola%20!Quiero%20conversar%20contigo!%20)
---
+++ app.py
@@ -0,0 +1,151 @@
+# Importación de bibliotecas necesarias
+import os
+import openai
+import streamlit as st
+import time
+
+import streamlit as st
+import openai
+
+# Configuración de la página
+st.set_page_config(
+    page_title="JusTicIA",
+    page_icon="🤖⚖️",
+    layout="wide",
+    menu_items={
+        'Get Help': 'https://marduk.pro',
+        'Report a bug': None,
+        'About': "JusTicIA: Tu asistente de IA para entender la transformación digital del sistema judicial en Colombia. Obtén información sobre proyectos, herramientas y el futuro de la IA en la justicia."
+    }
+)
+
+# Función para verificar si el archivo secrets.toml existe
+def secrets_file_exists():
+    secrets_path = os.path.join('.streamlit', 'secrets.toml')
+    return os.path.isfile(secrets_path)
+
+# Intentar obtener el ID del asistente de OpenAI desde st.secrets si el archivo secrets.toml existe
+if secrets_file_exists():
+    try:
+        ASSISTANT_ID = st.secrets['ASSISTANT_ID']
+    except KeyError:
+        ASSISTANT_ID = None
+else:
+    ASSISTANT_ID = None
+
+# Si no está disponible, pedir al usuario que lo introduzca
+if not ASSISTANT_ID:
+    ASSISTANT_ID = st.sidebar.text_input('Introduce el ID del asistente de OpenAI', type='password')
+
+# Si aún no se proporciona el ID, mostrar un error y detener la ejecución
+if not ASSISTANT_ID:
+    st.sidebar.error("Por favor, proporciona el ID del asistente de OpenAI.")
+    st.stop()
+
+assistant_id = ASSISTANT_ID
+
+# Inicialización del cliente de OpenAI
+client = openai
+
+st.title("Bienvenido a JusTicIA 🤖⚖️")
+
+st.markdown("""
+### 🤖 ¡Hola! Soy JusTicIA, tu guía en la transformación digital de la justicia colombiana
+
+Estoy aquí para brindarte información sobre cómo la Inteligencia Artificial (IA) está revolucionando el sistema judicial en Colombia.
+
+#### ¿Qué puedo hacer por ti hoy? 🤔
+
+* Explicarte cómo la IA se está utilizando para modernizar la justicia.
+* Describir los beneficios y desafíos de implementar la IA en el sistema judicial.
+* Analizar el impacto de herramientas de IA en la eficiencia y accesibilidad de la justicia.
+* Discutir consideraciones éticas y legales relacionadas con el uso de la IA en la justicia.
+* Explorar el futuro de la IA en el sistema judicial colombiano y su potencial transformador.
+
+**¡No dudes en preguntarme cualquier cosa sobre la IA y su papel en la justicia colombiana!**
+
+*Recuerda: Proporciono información basada en mi conocimiento actual. Para asesoramiento legal o técnico específico, consulta a un experto.*
+""")
+
+# Inicialización de variables de estado de sesión
+st.session_state.start_chat = True
+if "thread_id" not in st.session_state:
+    st.session_state.thread_id = None
+
+# Cargar la clave API de OpenAI
+API_KEY = os.environ.get('OPENAI_API_KEY') or st.secrets.get('OPENAI_API_KEY')
+if not API_KEY:
+    API_KEY = st.sidebar.text_input('Introduce tu clave API de OpenAI', type='password')
+
+if not API_KEY:
+    st.sidebar.error("Por favor, proporciona una clave API para continuar.")
+    st.stop()
+
+openai.api_key = API_KEY
+
+def process_message_with_citations(message):
+    """Extraiga y devuelva solo el texto del mensaje del asistente."""
+    if hasattr(message, 'content') and len(message.content) > 0:
+        message_content = message.content[0]
+        if hasattr(message_content, 'text'):
+            nested_text = message_content.text
+            if hasattr(nested_text, 'value'):
+                return nested_text.value
+    return 'No se pudo procesar el mensaje'
+
+# Crear un hilo de chat inmediatamente después de cargar la clave API
+if not st.session_state.thread_id:
+    thread = client.beta.threads.create()
+    st.session_state.thread_id = thread.id
+    st.write("ID del hilo: ", thread.id)
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("¿Cómo puedo ayudarte hoy?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("usuario"):
+        st.markdown(prompt)
+
+    # Enviar mensaje del usuario
+    client.beta.threads.messages.create(
+        thread_id=st.session_state.thread_id,
+        role="user",
+        content=prompt
+    )
+
+    # Crear una ejecución para el hilo de chat
+    run = client.beta.threads.runs.create(
+        thread_id=st.session_state.thread_id,
+        assistant_id=assistant_id
+    )
+
+    while run.status != 'completed':
+        time.sleep(1)
+        run = client.beta.threads.runs.retrieve(
+            thread_id=st.session_state.thread_id,
+            run_id=run.id
+        )
+
+    # Recuperar mensajes agregados por el asistente
+    messages = client.beta.threads.messages.list(
+    thread_id=st.session_state.thread_id
+    )
+
+    # Procesar y mostrar mensajes del asistente
+    for message in messages:
+        if message.run_id == run.id and message.role == "assistant":
+            full_response = process_message_with_citations(message)
+            st.session_state.messages.append({"role": "assistant", "content": full_response})
+            with st.chat_message("assistant"):
+                st.markdown(full_response)
+                
+# Footer
+st.sidebar.markdown('---')
+st.sidebar.subheader('Creado por:')
+st.sidebar.markdown('Alexander Oviedo Fadul')
+st.sidebar.markdown("[GitHub](https://github.com/bladealex9848) | [Website](https://alexanderoviedofadul.dev/) | [LinkedIn](https://www.linkedin.com/in/alexander-oviedo-fadul/) | [Instagram](https://www.instagram.com/alexander.oviedo.fadul) | [Twitter](https://twitter.com/alexanderofadul) | [Facebook](https://www.facebook.com/alexanderof/) | [WhatsApp](https://api.whatsapp.com/send?phone=573015930519&text=Hola%20!Quiero%20conversar%20contigo!%20)")
